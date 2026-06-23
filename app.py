471775f5fbd0214f82344617da2b477cbbfd6292
unknown <harishma1728@gmail.com>
2026-06-24 02:36:22 +0530

    Updated Groq model

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ Text:
 """
 
     response = client.chat.completions.create(
-        model="llama3-8b-8192",
+        model="llama-3.1-8b-instant"
         messages=[
             {
                 "role": "user",
